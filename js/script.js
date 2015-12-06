921c56a2472a8364b1f0f0fca92bc8bf8350cffe
annamarieboone <annamarie.boone@gmail.com>
2015-12-06 17:15:16 -0500

    refugee site

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 
-$(".slidingDiv").hide();
-$(".show_hide").show();
+var $window = $(window); //You forgot this line in the above example
 
-$('.show_hide').click(function(){
-$(".slidingDiv").slideToggle();
+$('section[data-type="background"]').each(function(){
+var $bgobj = $(this); // assigning the object
+$(window).scroll(function() {
+var yPos = -( ($window.scrollTop() + 400) / $bgobj.data('speed'));
+// Put together our final background position
+var coords = '50% '+ yPos + 'px';
+// Move the background
+//$bgobj.css({ backgroundPosition: coords });
+});
+});
 });
 
-});
+// Create HTML5 elements for IE
+                     
+                    document.createElement("article");
+                    document.createElement("section");
